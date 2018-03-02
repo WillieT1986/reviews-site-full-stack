085ae6951980e08cde14830d9159b6065b9d151a
WillieT1986 <WarMad2@Hotmail.com>
2018-03-02 10:39:39 -0500

    Added JpaWiringTest, test passed. Started JpaMappingTest...

---
+++ src/test/java/org/wecancodeit/columbus/reviewssitefullstack/JpaMappingTest.java
@@ -0,0 +1,16 @@
+package org.wecancodeit.columbus.reviewssitefullstack;
+
+import javax.annotation.Resource;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@DataJpaTest
+public class JpaMappingTest {
+
+	@Resource
+	private TestEntityManager entityManager;
+}
