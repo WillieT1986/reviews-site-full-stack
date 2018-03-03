32f2173467571f7dbbe2a764ce62c9c027940d87
WillieT1986 <WarMad2@Hotmail.com>
2018-03-03 15:27:05 -0500

    Added thymleaf info to categories.html and category.html and modeling them from reviews-site project html files from weekend 5 project. Modeled the controlled from reviews-site project as well. Ran Tests, tests still passed.

--- src/main/java/org/wecancodeit/columbus/reviewssitefullstack/ReviewController.java
+++ src/main/java/org/wecancodeit/columbus/reviewssitefullstack/ReviewController.java
@@ -1,8 +1,34 @@
 package org.wecancodeit.columbus.reviewssitefullstack;
 
+import javax.annotation.Resource;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ReviewController {
+
+	@Resource
+	private ReviewRepository reviewRepo;
+
+	@Resource
+	private CategoryRepository categoryRepo;
+
+	@Resource
+	private TagRepository tagRepo;
+
+	@RequestMapping(value = "reviews")
+	public String getAllReviews(Model model) {
+		model.addAttribute("reviews", reviewRepo.findAll());
+		return "reviews";
+	}
+
+	@RequestMapping("review")
+	public String getAReview(@RequestParam Long id, Model model) {
+		model.addAttribute("reviews", reviewRepo.findOne(id));
+		return "review";
+	}
 
 }
