1193542f146515fde04231ad60f80ee33a9adecb
WillieT1986 <WarMad2@Hotmail.com>
2018-03-02 19:12:03 -0500

    Added @Repository to ReviewRepository

--- src/main/java/org/wecancodeit/columbus/reviewssitefullstack/ReviewController.java
+++ src/main/java/org/wecancodeit/columbus/reviewssitefullstack/ReviewController.java
@@ -1,5 +1,12 @@
 package org.wecancodeit.columbus.reviewssitefullstack;
 
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class ReviewController {
 
+	@Resource
+	ReviewRepository reviewRepo = new ReviewRepository();
 }
--- src/main/java/org/wecancodeit/columbus/reviewssitefullstack/ReviewRepository.java
+++ src/main/java/org/wecancodeit/columbus/reviewssitefullstack/ReviewRepository.java
@@ -1,7 +1,9 @@
 package org.wecancodeit.columbus.reviewssitefullstack;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ReviewRepository extends CrudRepository<Review, Long> {
 
 }
