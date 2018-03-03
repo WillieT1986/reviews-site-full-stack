071e08902a9a52f579099f3075aa021fec4182fa
WillieT1986 <WarMad2@Hotmail.com>
2018-03-02 19:26:56 -0500

    ReviewController Class has an error and forces the Application Class to fail in TDD. Comment out code within ReviewController and Test Passes

--- src/main/java/org/wecancodeit/columbus/reviewssitefullstack/ReviewController.java
+++ src/main/java/org/wecancodeit/columbus/reviewssitefullstack/ReviewController.java
@@ -1,12 +1,22 @@
 package org.wecancodeit.columbus.reviewssitefullstack;
 
-import javax.annotation.Resource;
-
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class ReviewController {
 
-	@Resource
-	ReviewRepository reviewRepo = new ReviewRepository();
+	// @Resource
+	// ReviewRepository reviewRepo = new ReviewRepository();
+
+	// @RequestMapping(value = "reviews")
+	// public String getAllReviews(Model model) {
+	// model.addAttribute("reviews", reviewRepo.findAll());
+	// return "reviews";
+	// }
+	//
+	// @RequestMapping("review")
+	// public String getAReview(@RequestParam Long id, Model model) {
+	// model.addAttribute("reviews", reviewRepo.findOne(id));
+	// return "review";
+	// }
 }
